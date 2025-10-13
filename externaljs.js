c407dd704ab3908613273fadf07fae3cd55fd944
poojaKate16 <poojagavali1666@gmail.com>
2025-10-13 16:10:47 +0530

    Add files via upload

---
+++ externaljs.js
@@ -0,0 +1,19 @@
+function printnumber(a)
+{
+    document.write("<br>" +a);
+}
+
+
+printnumber(100);
+printnumber("pooja");
+
+
+function total(a,b)
+{
+var c=a+b;
+return c;
+
+}
+
+var k=total(11,11);
+document.write("<br>" +k);
